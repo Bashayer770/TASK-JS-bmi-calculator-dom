584a3116b8f238cae97cdcee33e68949a1641f35
bashayer770 <beshow95@gmail.com>
2023-05-10 20:10:05 +0300

    bmi

--- index.js
+++ index.js
@@ -7,47 +7,44 @@ function calculateBMI() {
   let female = document.getElementById("f").checked;
   console.table({ age, weight, height, male, female });
 
-  // You are going to write the code here before the curly braces 👇🏻
-  let BMI = weight/(height*height*0.0001)
+  // You are going to write the code here before the curly braces. 👇🏻
+  let BMI = weight / (height * height * 0.0001);
 
-  if (BMI < 18.5){
-    alert(`underweight ${BMI} ${height} ${weight}`)
-  } else if (BMI > 18.5 && BMI < 24.9 ){
-  alert("Healthy weight")
-} else if (BMI > 25.0 && BMI < 29.9){
-    alert("overweight")
-  } else if (BMI > 30 && BMI > 30.0) { 
-    alert(`Obesity ${BMI} ${height} ${weight}`)
+  if (BMI < 18.5) {
+    alert(`underweight ${BMI} ${height} ${weight}`);
+  } else if (BMI > 18.5 && BMI < 24.9) {
+    alert("Healthy weight");
+  } else if (BMI > 25.0 && BMI < 29.9) {
+    alert("overweight");
+  } else if (BMI > 30 && BMI > 30.0) {
+    alert(`Obesity ${BMI} ${height} ${weight}`);
   }
 
-
-  if (age >= 19 && age >=24  ){
-    if(BMI > 19 && BMI < 24 ){
-      console.log("normal weihth")
-    } else if (BMI < 19){ 
-      console.log("underweight")
-    } else if (BMI > 24){
-      console.log("Overwieght")
+  if (age >= 19 && age >= 24) {
+    if (BMI > 19 && BMI < 24) {
+      console.log("normal weihth");
+    } else if (BMI < 19) {
+      console.log("underweight");
+    } else if (BMI > 24) {
+      console.log("Overwieght");
     }
-    
-  } else if (age>= 25 && age <= 34 ){
-    if(BMI > 18.5 && BMI < 24.9 ){
-      console.log("normal weihth")
-    } else if (BMI < 18.5){ 
-      console.log("underweight")
-    } else if (BMI > 24.9){
-      console.log("Overwieght")
+  } else if (age >= 25 && age <= 34) {
+    if (BMI > 18.5 && BMI < 24.9) {
+      console.log("normal weihth");
+    } else if (BMI < 18.5) {
+      console.log("underweight");
+    } else if (BMI > 24.9) {
+      console.log("Overwieght");
     }
-  
-  
-} else if (age>= 35 && age <= 44 ){
-  let BMI_START = 21
-  let BMI_END = 26
-  if(BMI >= BMI_START && BMI <= BMI_END){
-    alert("normal weihth")
-  } else if (BMI <BMI_START){ 
-    console.log("underweight")
-  } else if (BMI > BMI_END){
-    console.log("Overwieght")
+  } else if (age >= 35 && age <= 44) {
+    let BMI_START = 21;
+    let BMI_END = 26;
+    if (BMI >= BMI_START && BMI <= BMI_END) {
+      alert("normal weihth");
+    } else if (BMI < BMI_START) {
+      console.log("underweight");
+    } else if (BMI > BMI_END) {
+      console.log("Overwieght");
+    }
   }
-}}
+}
